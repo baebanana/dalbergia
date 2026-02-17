8f363761ccdb1348a8ffa1e37fe7f1463985ac49
game <urbae0001s@gmail.com>
2026-02-17 13:36:59 +0700

    fix urls match views

--- dataapp/urls.py
+++ dataapp/urls.py
@@ -2,54 +2,25 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # ================= HOME =================
     path('', views.home, name='home'),
     path('homepage/', views.home, name='homedata'),
 
-    # ================= MANAGE =================
     path('manage_data/', views.managedata, name='mndata'),
     path('manage_genus/', views.managegenus, name='genusdata'),
     path('manage_species/', views.managespeci, name='specidata'),
     path('manage_info/', views.manageinfo, name='infodata'),
 
-    # ================= GENUS =================
     path('genus_add/', views.addgenus, name='addgenu'),
-    path('genus_delete/<int:genu_id>/', views.genusdelete, name='genusde'),
+    path('genus_delete/<genu_id>/', views.genusdelete, name='genusde'),
     path('genus_update/<int:gn_id>/', views.genusupdate, name='updatege'),
     path('genus_search/', views.genussearch, name='genusearch'),
 
-    # ================= SPECIES =================
     path('species_add/', views.addspecies, name='addspec'),
-    path('species_delete/<int:spec_id>/', views.deletespecies, name='speciesdelete'),
+    path('species_delete/<spec_id>/', views.deletespecies, name='speciesdelete'),
     path('species_update/<int:spec_id>/', views.updatespecies, name='speciesupdate'),
     path('species_search/', views.searchspecies, name='speciessearch'),
-    path('species_image/', views.imagespeciesupload, name='speciesimage'),
-    path('species_show_image/<int:image_id>/', views.speciesgallery, name='showspeciesgallery'),
-    path('species_page/', views.speciesdata, name='pagespecies'),
-    path('testadd/', views.testaddspecies, name='addtest'),
-    path('download-images/', views.downloadselectedimages, name='download_selected_images'),
-    path('testload_image/<int:image_id>/', views.testloadgallery, name='testload'),
-    path('species_detail_page/<int:pk>/', views.speciesfulldetail, name='species_full_detail'),
 
-    # ================= INFO =================
-    path('info_add/', views.addinfomation, name='addinf'),
-    path('info_delete/<int:k_id>/', views.deleteinfomation, name='knowdelete'),
-    path('info_update/<int:k_id>/', views.updateinfomation, name='knowupdate'),
-    path('info_search/', views.searchinfo, name='infosearch'),
-    path('knowledge_detail/<int:pk>/', views.knowledgedetail, name='knowledge_detail'),
-
-    # ================= AI =================
     path('classify_page/', views.predictplant, name='classify'),
-    path('add_model/', views.addmodel, name='addmodel'),
-    path('delete_model/<str:filename>/', views.delete_model, name='lostmodel'),
 
-    # ================= SEARCH =================
-    path('species_show_search/', views.searchspecies2, name='speciessearch2'),
-
-    # ================= ADMIN =================
-    path('login_form/', views.adminlogincode, name='formlogin'),
-    path('add_admin/', views.addadmincode, name='adminadd'),
-    path('manage_addmin/', views.manageadmin, name='adminmanage'),
-    path('delete_admin/<int:admin_id>/', views.deleteadmin, name='admindelete'),
-    path('logout/', views.admin_logout, name='adminlogout'),
+    path('login_form/', views.adminlogin, name='formlogin'),
 ]
